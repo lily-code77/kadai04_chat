db5940d37adccb0564920422b0700ecaae457976
stotoki <totoki.sayoko@gmail.com>
2024-03-08 21:13:37 +0900

    changed firebase db

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ import firebaseConfig from "./firebaseApikey.js";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getDatabase, ref, push, set, onChildAdded, remove, onChildRemoved }
+import { getDatabase, ref, push, set, onChildAdded }
     from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,7 +14,7 @@ console.log(firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const dbRef = ref(db, "Happy Chat");
+const dbRef = ref(db, "happyChat");
 
 //送信ボタンのクリックイベントの作成
 let inputMsg = "";
