79695badfe025f95b1f76508cdccbd10ed4869e0
stotoki <totoki.sayoko@gmail.com>
2024-03-08 21:19:47 +0900

    rfq

--- js/main.js
+++ js/main.js
@@ -9,8 +9,6 @@ import { getDatabase, ref, push, set, onChildAdded }
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-console.log(firebaseConfig);
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
